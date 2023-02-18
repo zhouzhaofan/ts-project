623dab8a084de35134eb876a8022ab53103f1d7e
zhouzhaofan <892747218@qq.com>
2023-02-18 19:10:48 +0800

    kkk

--- test.ts
+++ test.ts
@@ -1,11 +1,11 @@
-type Xargs= {
+interface Xargs {
   name: string
   age: number
 }
 
 export function test(args: Xargs): void {
-  const a = args.name;
-  const b = args.age;
+  const a = args.name
+  const b = args.age
   const objs = [
     { a: 'XXXXXXX', b: 'yyyyy', c: 'zzzzz' },
     { a: 'XXXXXXX', b: 'yyyyy', c: 'zzzzz' },
@@ -14,10 +14,12 @@ export function test(args: Xargs): void {
     { a: 'XXXXXXX', b: 'yyyyy', c: 'zzzzz' }
   ]
 
+  console.log('----', objs)
+
   const set = new Set([1, 2, 3, 4, 5])
   const arr = [...set]
   console.log(arr.length)
-  console.log('--ts-standard--' + a + ' ' + b)
+  console.log(`--ts-standard-- ${a} ${b} `)
   console.log(objs)
   ;[1, 2, 3].forEach((v) => {
     console.log(v)
